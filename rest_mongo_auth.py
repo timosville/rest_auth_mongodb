ca45771f5579105cc02dd2612d3ca4c5bac4876a
timosville <timosville@gmail.com>
2016-09-06 09:17:55 +0300

    Password authentication complete

--- rest_mongo_auth.py
+++ rest_mongo_auth.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from mongoframes import Frame
 from pymongo import MongoClient
-from flask import request, abort, jsonify, url_for
+from flask import request, abort, jsonify, url_for, g
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ MONGO_URI = 'mongodb://localhost:27017/' + DB_NAME
 
 app.mongo = MongoClient(MONGO_URI)
 Frame._client = app.mongo
+auth = HTTPBasicAuth()
 
 
 class User(Frame):
@@ -44,6 +46,21 @@ def get_user(_id):
     if not user:
         abort(400)
     return jsonify({'username': user.username})
+
+
+@auth.verify_password
+def verify_password(username, password):
+    user = User.one({'username': username})
+    if not user or not check_password_hash(user.password_hash, password):
+        return False
+    g.user = user
+    return True
+
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, {}!'.format(g.user.username)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
